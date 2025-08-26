819cf21b8805fd43cb197b06c5a146512c28cebd
Amit Kumar <amit2610tanwar@gmail.com>
2025-08-26 18:37:36 +0530

    Add reverse with loop and recursion

--- reverse_of_array_using_recursion.cpp
+++ reverse_of_array_using_recursion.cpp
@@ -1,20 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void reverse_of_array(int arr[], int size) {
-    int l = 0; int r = size - 1;
-    for ( l=0; l<=r; l++){
+void reverse_of_array_using_for_loop(int l, int arr[], int r) {
+    
+    for ( l; l<=r; l++){
         swap(arr[l],arr[r]);
         r--;
     }
 }
 
+void reverse_using_recusion(int l, int arr[], int r ){
+   if (l>=r) return;
+   swap(arr[l], arr[r]);
+   reverse_using_recusion(l+1, arr, r-1);
+
+
+}
+
 int main(){
     int arr[] = {3,4,6,7,2};
-    int size = sizeof(arr)/sizeof(arr[0]);
-    reverse_of_array(arr, size);
+    int r = sizeof(arr)/sizeof(arr[0]) - 1;
+    // reverse_of_array_using_for_loop(0,arr, r);
+    // reverse_using_recusion (0, arr, r);
     cout << "Reversed array: ";
-    for (int i=0; i<size; i++){
+    for (int i=0; i <= r; i++){
         cout << arr[i] << " ";
     }
 }
