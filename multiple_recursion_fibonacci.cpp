d5c16785e65aff8386e21885ea489f65d5f9bcfe
Amit Kumar <amit2610tanwar@gmail.com>
2025-08-29 02:12:01 +0530

    Add Comment for DP in fibonacci

--- multiple_recursion_fibonacci.cpp
+++ multiple_recursion_fibonacci.cpp
@@ -19,4 +19,7 @@ int main()
     cout << result << "\n";
 }
 
-//This is code is only good for small values of n, it has time complexity of 2^n. Thus there are better ways to calulate fibonacci with much more efficiency. 
+/*This is code is only good for small values of n, it has time complexity of 2^n. Thus there are better ways to calulate fibonacci with much more efficiency. 
+We can use DP (Dynamic programming) to reduce the time complexity by a lot. Just make an array dp and store all the computed values of f(n) into it,
+always check if f(n) is available in dp before calculating it. Will commit a fibonacci program with dp in it.
+*/
